3867a6800ffa9e1b4a27ba701778d7fb97ddaa78
Olga Kopyova <imkopyova@gmail.com>
2020-11-23 11:36:39 +0300

    fix: refactor form submit

--- static/common/getFieldsValues.js
+++
@@ -1,6 +0,0 @@
-function getFieldsValues(form) {
-    const fieldsList = form.querySelectorAll("input");
-    for (field of fieldsList) {
-        console.log(field.value);
-    }
-}
--- static/common/getTextareaValues.js
+++
@@ -1,6 +0,0 @@
-function getTextareaValues(form) {
-    const fieldsList = form.querySelectorAll("textarea");
-    for (field of fieldsList) {
-        console.log(field.value);
-    }
-}
