0ae85416128bd01ebba9d938bcd4bad7650764b7
Olga Kopyova <imkopyova@gmail.com>
2021-01-29 18:03:34 +0300

    feat: validation

--- src/helpers/submitForm.ts
+++ src/helpers/submitForm.ts
@@ -1,14 +1,55 @@
 const forms = document.forms;
 
-function onSubmit(e: Event) {
-    e.preventDefault();
-    console.log(e.target)
-    let formData = new FormData(e.target);
+const validations: {[key: string]: RegExp} = {
+    email: /^\S+@\S+$/i,
+    tel: /\+7\s\([0-9]{3}\)\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}/i,
+    text: /\w+/,
+    password: /\w+/,
+}
+
+function onSubmit(event: Event) {
+    event.preventDefault();
+    const element = <HTMLFormElement>event.target;
+    let formData = new FormData(element);
     for (let [name, value] of formData) {
         console.log(`${name}: ${value}`)
     }
 }
 
+function onFocus(field: Element) {
+    field.classList.remove("field-state-error");
+}
+
+function onBlur(event: Event, field: Element) {
+    const isValid = validateInput(<HTMLInputElement>event.target);
+    if (!isValid) {
+        field.classList.add("field-state-error");
+    }
+}
+
+function validateInput(input: HTMLInputElement): boolean {
+    const type = input.type;
+    const value = input.value;
+
+    let regexp = /\w+/;
+    if (type in validations) {
+        regexp = validations[type];
+    }
+
+    return regexp.test(value);
+}
+
+function addBlurFocusListener(field: Element) {
+    const input = <HTMLInputElement>field.querySelector("input");
+    input.addEventListener("focus", () => onFocus(field));
+    input.addEventListener("blur", e => onBlur(e, field));
+}
+
 for (let form of forms) {
+    const fields = document.querySelectorAll(".field");
+    for (let field of fields) {
+        addBlurFocusListener(field);
+    }
+
     form.addEventListener("submit", onSubmit);
 }
